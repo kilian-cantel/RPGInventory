dc9f11f7178fc2217047c1697cb470764f749a61
kilian-cantel <kiliancantel@gmail.com>
2025-09-04 01:57:45 +0200

    Add configuration files and utility methods to RPGInventory plugin

--- src/main/java/com/neko/RPGInventory.java
+++ src/main/java/com/neko/RPGInventory.java
@@ -1,17 +1,30 @@
 package com.neko;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RPGInventory extends JavaPlugin {
 
+    private static RPGInventory instance;
+
     @Override
     public void onEnable() {
-
+        saveDefaultConfig();
+        instance = this;
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static RPGInventory getInstance() {
+        return instance;
+    }
+
+    public static void sendConsoleMessage(Component msg) {
+        Bukkit.getConsoleSender().sendMessage(msg);
     }
 }
 
