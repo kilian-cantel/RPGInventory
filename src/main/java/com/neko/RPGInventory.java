5f1cd480b4b29652fc88db5ede2d36caf27dea46
kilian-cantel <kiliancantel@gmail.com>
2025-09-03 22:46:36 +0200

    Add initial project setup with Maven configuration and plugin boilerplate

---
+++ src/main/java/com/neko/RPGInventory.java
@@ -0,0 +1,17 @@
+package com.neko;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RPGInventory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
+
